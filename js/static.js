432e499499fc185cc80e7d2440488cf83a95463b
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-05-04 13:47:11 +0300

    ;) add slider

---
+++ js/static.js
@@ -0,0 +1,24 @@
+  let size = 0;
+  const sliderItemsNode = document.querySelector('.header__content-items--js')
+  let arrowleftNode = document.querySelector('.arrow-left--js')
+  let arrowrightNode = document.querySelector('.arrow-right--js')
+  arrowleftNode.addEventListener('click', function() {
+      size += 589
+      if (size > 1767) {
+          size = 0
+      }
+
+      sliderItemsNode.style.left = -size + 'px'
+
+
+  })
+  arrowrightNode.addEventListener('click', function() {
+      size += -589
+      if (size < 0) {
+          size = 1767
+      }
+
+      sliderItemsNode.style.left = -size + 'px'
+
+
+  })
