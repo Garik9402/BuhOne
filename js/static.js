94eee6af0b4aea43e9678a2ff6f87edb575cd81f
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-05-06 15:20:24 +0300

    add:)

--- js/static.js
+++ js/static.js
@@ -2,14 +2,18 @@
   const sliderItemsNode = document.querySelector('.header__content-items--js')
   let arrowleftNode = document.querySelector('.arrow-left--js')
   let arrowrightNode = document.querySelector('.arrow-right--js')
+  const dotsNode = document.querySelectorAll(".dots--js")
+  let index = 0;
+
+
+
   arrowleftNode.addEventListener('click', function() {
       size += 589
       if (size > 1767) {
           size = 0
       }
 
-      sliderItemsNode.style.left = -size + 'px'
-
+      sliderItemsNode.style.left = -size + 'px';
 
   })
   arrowrightNode.addEventListener('click', function() {
@@ -18,7 +22,7 @@
           size = 1767
       }
 
-      sliderItemsNode.style.left = -size + 'px'
+      sliderItemsNode.style.left = -size + 'px';
 
 
   })
