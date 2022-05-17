7aeea4f810d121ff72427db9b280d9be4b0649b3
Garik9402 <101651468+Garik9402@users.noreply.github.com>
2022-05-17 13:28:33 +0300

    add slider:)

--- js/static.js
+++ js/static.js
@@ -3,6 +3,19 @@ $(document).ready(function() {
         arrows: true,
         dots: true,
         speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 3000,
+
+    });
+
+});
+$(document).ready(function() {
+    $('.clients-slider').slick({
+        arrows: true,
+        dots: true,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 3000,
 
     });
 
